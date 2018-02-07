709fa5cce76e2a0735c6c028c2d340e5269e2d49
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-07 10:23:58 -0300

    ejercicios 6 7 8 9 y 10
    utilizamos el parseInt , cuentas simples (+-/*) y aumento y descuento de porcentaje

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,18 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	//definimos variable
+	var numeroUno;
+	var numeroDos;
+	var resultado;
 
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);// usamos el parseInt para convertir en numero las palabras para sumar 
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	resultado=numeroUno+numeroDos;
+
+	alert("la suma es: "+resultado);
 }
 
