1af3b2ab7d476ea0b439e497cc0f314d137a6168
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-12 15:42:12 -0300

    ejercicio 4, 5 y 6
    aprendimos a usar el condicional: if, else, &&, etc

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,16 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
 
+	edad=document.getElementById('edad').value;
+
+	if(edad>12)
+	{
+		if (edad<18)
+		{
+			alert("es adolecente");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
+	
+	var edad;
 
+	edad=document.getElementById('edad').value;
+
+	if (!(edad>12 && edad<18))
+	{
+		alert ("no es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
