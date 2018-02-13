772a7601373132aefc788650ed3365366321d747
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-13 17:11:55 -0300

    parciales 2 y 3
    el 2 no se entendió por que hay un cuadro para escribir y el tres es casi igual a tp 1

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,12 @@
 function Mostrar()
 {
-  
+  var iva;
+  var producto;
+  var importe;
+
+  iva=1.21;
+  producto=prompt("ingrese el precio del producto","0");
+  producto=parseInt(producto);
+  importe=producto*iva;
+  document.getElementById('importeFinal').value=importe;
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,17 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
 
+	largo=document.getElementById('alrgo').value;
+
+	ancho=document.getElementById('ancho').value;
+
+	perimetro=largo*2+ancho*2;
+
+	alambre=perimetro/3;
+
+	alert("el perimetro es de "+perimetro+" mts por lo tanto se nesecitan "+alambre+" mts de alambre");
 }
