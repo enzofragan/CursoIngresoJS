9564405464607d03c8ba76ab823ee6f30c222d04
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-13 17:45:48 -0300

    ejercicio 6 y 7
    ejercicios en los que use doble if y if-else-if
    nota: no hice el 9 y 10 por que era de numero al azar y no sabia como

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,19 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var edad;
+	var estado;
+
+	edad=document.getElementById('edad').value;
+	estado=document.getElementById('estadoCivil').value;
+
+	if (edad<18)
+	{
+		if (!(estado=="Soltero"))
+		{
+			alert("Es muy pequeño para NO ser soltero.");
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
