28e07435623982081188fee61e63cff910e7d8b8
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-14 10:34:39 -0300

    ejercicio 9 y 10
    aprendimos a usar numeros random

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,37 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota;
+
+	nota=Math.floor(Math.random()*10)+1;
+
+	/*if(nota>8)
+	{
+		alert(nota+" EXCELENTE");
+	}
+	else
+	{
+		if (nota>4)
+		{
+			alert(nota+" APROBÓ");
+		}
+		else
+		{
+			alert(nota+" Vamos, la proxima se puede");
+		}
+	}*/
+
+	if(nota>8)
+	{
+		alert("EXCELENTE");
+	}
+		if(nota>4 && nota<8)
+		{
+			alert("APROBÓ")
+		}
+			if(nota<4)
+			{
+				alert("Vamos, la proxima se puede");
+			}
 
 }//FIN DE LA FUNCIÓN
