f525101da77d502f2cdc1722d4ae1ef534e95d34
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-14 11:54:58 -0300

    ejercicio 2, 3 y 4
    aprendimos un mensaje para varios case y default.
    nota: en el 2 falta el alert en el ultimo break.

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,10 +1,28 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+	switch(mesDelAño)
+	{
+		case "Febrero":
+			alert("tiene 28 dias");
+			break;
+
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+			alert("tiene 31 dias");
+			break;
+
+		default:
+			alert("tiene 30 dias");
+	}
 	
 
 
