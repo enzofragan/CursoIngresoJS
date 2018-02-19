74bb5951bda139dd508fad63b17a0267697a0ae4
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-19 19:54:03 -0300

    parcial r4
    resuelto falta aprender del document.write

--- 9-Parciales/recuperatorio/cuatro.js
+++ 9-Parciales/recuperatorio/cuatro.js
@@ -1,4 +1,27 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
 
+	numeroUno=prompt("ingrese el primer numero","0");
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=prompt("ingrese el segundo numero","0");
+	numeroDos=parseInt(numeroDos);
+
+	resultado=numeroUno+numeroDos;
+
+	if (resultado==0)
+	{
+		document.write("el resultado es cero");
+	}
+	else if (resultado>0)
+		{
+			document.write("el resultado es positivo");
+		}
+		else
+		{
+			document.write("el resultado es negativo");
+		}
 }
