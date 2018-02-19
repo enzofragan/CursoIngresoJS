dbe66c451daabaa222f6982b91a62cd6609bb135
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-19 19:52:48 -0300

    parcial 4
    esta resuelto aunque falta aprender mejor del document.write

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,28 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
 
+	numeroUno=prompt("ingrese el primer numero","0");
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=prompt("ingrese el segundo numero","0");
+	numeroDos=parseInt(numeroDos);
+
+	if (numeroUno==numeroDos)
+	{
+		resultado=numeroUno*numeroDos;
+		document.write(resultado);
+	}
+	else if (numeroUno>numeroDos)
+		{
+			resultado=numeroUno-numeroDos;
+			document.write(resultado);
+		}
+		else
+		{
+			resultado=numeroUno+numeroDos;
+			document.write(resultado);
+		}
 }
