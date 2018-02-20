125fdabf2c2c81cd183fff9d43a3b5d98d05b18f
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-20 09:37:05 -0300

    ejercicios 1 y 2
    aprendimos a usar el while con ascenso y descenso.

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
-	alert('iteración while');
+	var contador;
+	contador=10;
 
+	while(contador>0)
+	{
+		contador --;//es lo mismo que decir contador - 1 y lo ismo con el ++.
+		document.write("<br> "+contador);
+	}
 
 }//FIN DE LA FUNCIÓN
