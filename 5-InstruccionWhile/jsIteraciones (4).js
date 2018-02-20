320772afa47eb281dfc44fd5647a9238515b21e4
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-20 10:14:37 -0300

    ejercicio 3 y 4
    usamos el while pero acá pide clave y numero especifico, por lo que hay que pensar en forma contraria a la condicion

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,5 +3,10 @@ function Mostrar()
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
+	while(!(numero>-1 && numero<11))
+	{
+		numero = prompt("error, ingrese un número entre 0 y 10.");
+	}
+	document.getElementById('Numero').value=numero;
 
 }//FIN DE LA FUNCIÓN
