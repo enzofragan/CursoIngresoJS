d31b1858d626b30dadd98e1c6a08a20f3c6eb4f4
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-20 12:11:26 -0300

    ejercicio 6, 7, 8 y 9
    aprendimos a usar el while con cuentas y a para cuando el usuario lo quiera

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,11 +4,27 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numero;
 	
 	var respuesta='si';
 
+	while(respuesta=='si')
+	{
+		contador ++;
+		numero=prompt("ingrese un numero");
+		numero=parseInt(numero);
+		if (numero>0)
+		{
+			positivo=positivo+numero;
+		}
+		if (numero<0)
+		{
+			negativo=negativo*numero
+		}
+		respuesta=prompt("ingrese si para continuar");
+	}
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+	document.getElementById('suma').value=positivo;
+	document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,16 +3,41 @@ function Mostrar()
 
 	var contador=0;
 	// declarar variables
+	var max;
+	//var max=-999;
+	var min;
+	//var min=999;
+	var numero;
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
-	
+		contador++;
+		numero=prompt(" ingrese un numero");
+		numero=parseInt(numero);
+
+
+		if(contador==1)
+		{
+			max=numero;
+			min=numero;
+		}else
+		{
+			if(numero>max)
+			{
+				max=numero;
+			}
+			if(numero<min)
+			{
+				min=numero;
+			}
+		}
+
+		respuesta=prompt("ingrese si para continuar o no para terminar");
 	}
 
-
-
+	document.getElementById('maximo').value=max;
+	document.getElementById('minimo').value=min;
 
 }//FIN DE LA FUNCIÓN
