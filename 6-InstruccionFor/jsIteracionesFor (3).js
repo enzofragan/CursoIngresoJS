5015cc9d3528f87cfc581cdbe2c738c50b78011b
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-21 12:12:25 -0300

    ejercicio 2 y 3
    for con break y continue for con switch.

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,7 +1,73 @@
 function Mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+	//var repetciones = prompt("ingrese el número de repeticiones");
+	var iteraciones=1000;
+	var numeroRandom;
+	var contadorCero=0;
+	var contadorUno=0;
+	var contadorDos=0;
+	var contadorTres=0;
+	var contadorCuatro=0;
+	var contadorCinco=0;
+	var contadorSies=0;
+	var contadorSiete=0;
+	var contadorOcho=0;
+	var contadorNueve=0;
+	
+
+	for(contador=0;contador<iteraciones;contador ++);
+	{
+		numeroRandom=Math.floor(Math.random() * 10);
+
+		switch(numeroRandom)
+		{
+			case 0:
+				contadorCero ++;
+				break;
+			case 1:
+				contadorUno ++;
+				break;
+			case 2:
+				contadorDos ++;
+				break;
+			case 3:
+				contadorTres ++;
+				break;
+			case 4:
+				contadorCuatro ++;
+				break;
+			case 5:
+				contadorCinco ++;
+				break;
+			case 6:
+				contadorSies ++;
+				break;
+			case 7:
+				contadorSiete ++;
+				break;
+			case 8:
+				contadorOcho ++;
+				break;
+			case 9:
+				contadorNueve ++;
+				break;
+		}
+	}
+
+	document.write("<br> "+numeroRandom);
+	document.write("<br> cero: "+contadorCero);
+	document.write("<br> uno: "+contadorUno);
+	document.write("<br> dos: "+contadorDos);
+	document.write("<br> tres: "+contadorTres);
+	document.write("<br> cuatro: "+contadorCuatro);
+	document.write("<br> cinco: "+contadorCinco);
+	document.write("<br> seis: "+contadorSies);
+	document.write("<br> siete: "+contadorSiete);
+	document.write("<br> ocho: "+contadorOcho);
+	document.write("<br> nueve: "+contadorNueve);
+
+
 
 
 
