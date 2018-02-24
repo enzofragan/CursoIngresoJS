7d9e40c4fbbb03ca26cab0044936d922a2038906
enzo <36193661+enzofragan@users.noreply.github.com>
2018-02-24 12:18:03 -0300

    parcial 6 y 8
    los dos con while pero el 8 con un enunciado diferente

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,83 @@
+/*ingresar los alumnos pedidiendo el nombre (en el pzarron). ¿cuantos alumno ingrese?, y pedir la nota, ¿cual es el promedio?
+pedri el sexo y la cantidad de ombre y mujeres y pedir qe varones desaprobaron, y el nombre de la mejor nota, 
+promedio de las mujeres, sexo de la menor edad, promedio de las edades de los aprobados, cantidad de notas pares,
+cantidad de notas impares  */
 function Mostrar()
 {
+	var alumnos;
+	var notas=0;
+	var acumulador=0;
+	var contador=0;
+	var promedio;
+	var sexo;
+	var cantF=0;
+	var cantM=0;
+	var max=0;
+	var mejorNota;
+	var acumuladorF=0;
+	var promedioF;
+	var edad;
+	var min=100;
+	var menorEdad;
+	var respuesta='si';
 
+	while(respuesta!='no')
+	{
+		contador ++;
+		alumnos=prompt("ingrese el nombre");
+		notas=prompt("ingrese su nota");
+		notas=parseInt(notas);
+		while(notas<0 || notas>10)
+		{
+			notas=prompt("ingrese la nota correcta");
+			notas=parseInt(notas);
+		}
+			sexo=prompt("ingrese el sexo f(para mujer) y m(para hombre)");
+		while(sexo!="f" && sexo!="m")
+		{
+			sexo=prompt("ingrese el sexo correcto");
+		}
+		if (sexo=="f")
+		{
+			cantF ++;
+			acumuladorF=acumuladorF+notas;
+		}
+		if (sexo=="m")
+		{
+			cantM ++;
+		}
+		if(notas>max)
+		{
+			max=notas;
+			mejorNota=alumnos;
+		}
+
+		edad=prompt("ingrese la edad","0");
+		edad=parseInt(edad);
+
+		while(edad<0 || isNaN(edad))
+		{
+		edad=prompt("ingrese la edad correcta","0");
+		edad=parseInt(edad);
+		}
+
+		if(edad<min)
+		{
+			min=edad;
+			menorEdad=sexo;
+		}
+		acumulador=acumulador+notas;
+		document.write("<br> "+alumnos+" su sexo es "+sexo+" se saco "+notas);
+		respuesta=prompt("ingrese no para finalizar");
+
+	}
+	promedio=acumulador/contador;
+	promedioF=acumuladorF/cantF;
+	document.write("<br> se ingresaron: "+contador+" alumnos");
+	document.write("<br> el promedio de las notas es de: "+promedio);
+	document.write("<br> la cantidad de mujeres es: "+cantF);
+	document.write("<br> la cantidad de hombres es: "+cantM);
+	document.write("<br> el alumno con la mejor nota es: "+mejorNota);
+	document.write("<br> el promedio de las mujeres es de: "+promedioF);
+	document.write("<br> el sexo del menor es: "+menorEdad);
 }
--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,15 @@
 function Mostrar()
 {
+	var importe=1;
+	var dias=24;
+	var contador=0;
+
+	importe=prompt("ingrese importe","0");
+	importe=parseInt(importe);
+
+	while(contador<dias)
+	{
+		
+	}
 
 }
